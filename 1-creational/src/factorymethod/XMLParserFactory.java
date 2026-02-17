080441995a73e45dd40efa1e8ce4cabd98df39a0
Jake Atkins <atkins.jake@icloud.com>
2026-02-17 15:46:41 +0000

    done 1.factorymethod

--- 1-creational/src/factorymethod/XMLParserFactory.java
+++ 1-creational/src/factorymethod/XMLParserFactory.java
@@ -1,9 +1,18 @@
 package factorymethod;
 
+import abstractfactory.NYErrorXMLParser;
+import abstractfactory.NYFeedbackXMLParser;
+import abstractfactory.NYOrderXMLParser;
+
 public class XMLParserFactory {
 
     public XMLParser getParser(String xmlType) {
-        // TODO: implement
-        return null;
+        return switch (xmlType){
+            case "ORDER" -> new OrderXMLParser();
+            case "FEEDBACK" -> new FeedbackXMLParser();
+            case "ERROR" -> new ErrorXMLParser();
+            default -> null;
+        };
+
     }
 }
